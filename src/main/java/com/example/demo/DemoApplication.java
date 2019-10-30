f260ff1e9bf3045d008c1f8e07445f7d6505ee55
nklulupeterpan <yanglunk@gmail.com>
2019-10-30 19:21:16 +0000

    push for save

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,56 @@
 package com.example.demo;
 
-import com.example.demo.config.ApplicationConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.annotation.Router;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.http.config.EnableIntegrationGraphController;
+import org.springframework.integration.jms.JmsDestinationPollingSource;
+import org.springframework.integration.jms.JmsSendingMessageHandler;
+import org.springframework.integration.router.ExpressionEvaluatingRouter;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
+@SpringBootApplication
+@Configuration
+@EnableIntegration
+@IntegrationComponentScan("com.example.demo")
+@EnableIntegrationGraphController(allowedOrigins = "http://localhost:8082")
+@ImportResource("classpath:META-INF/spring/integration/orderProcess.xml")
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApplicationConfiguration.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
+
+//	@ServiceActivator(inputChannel="requestChannel", poller = @Poller(fixedRate = "30000"))
+//	@Bean
+//	public JmsDestinationPollingSource inboundJmsAdapter(JmsTemplate jmsTemplate) {
+//		JmsDestinationPollingSource source = new JmsDestinationPollingSource(jmsTemplate);
+//		source.setDestinationName("in.message.queue.name");
+//		return source;
+//	}
+//
+//	@Router(inputChannel = "inChannel")
+//	@Bean
+//	public ExpressionEvaluatingRouter router() {
+//		ExpressionEvaluatingRouter router = new ExpressionEvaluatingRouter("payload.colour");
+//		router.setChannelMapping("RED", "redChannel");
+//		router.setChannelMapping("YELLOW", "yellowChannel");
+//		router.setChannelMapping("GREEN", "greenChannel");
+//		return router;
+//	}
+
 }
+
+
+
--- src/main/java/com/example/demo/gateway/ReadJsonFromFileGateway.java
+++ src/main/java/com/example/demo/gateway/ReadJsonFromFileGateway.java
@@ -1,13 +1,16 @@
 package com.example.demo.gateway;
 
+import com.example.demo.domain.OrderItem;
+import org.springframework.integration.annotation.Filter;
 import org.springframework.integration.annotation.Gateway;
+import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.MessagingGateway;
 
-@MessagingGateway(name = "readJsonGateway", defaultRequestChannel = "readJsonRequestChannel", defaultReplyChannel = "readJsonReplyChannel")
+@MessagingGateway(name = "readJsonGateway", defaultRequestChannel = "readJsonRequestChannel",
+
+        defaultReplyChannel = "readJsonReplyChannel", defaultReplyTimeout = "10000")
 public interface ReadJsonFromFileGateway {
-
-        @Gateway
-        String processReadRequest();
+       @Gateway
+       public String processReadRequest();
 }
-
 
